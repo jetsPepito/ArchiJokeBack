039de8fef01ebc8b16390ef0172e9c50bdf2ee84
Wassim <wassim.ajili@epita.fr>
2021-10-18 16:47:51 +0200

    fix: Handle CORS error

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from endpoints import jokes, users
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(jokes.router)
 app.include_router(users.router)
